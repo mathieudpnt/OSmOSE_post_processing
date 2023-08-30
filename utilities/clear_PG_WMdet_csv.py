c2294135b96f817195d5c07af356f4fc3ccc1fe1
Maëlle TORTEROTOT <torterma@ensieta.ecole>
2023-08-30 16:51:05 +0200

    clean_PG_WMder_csv.py added in utilities

---
+++ utilities/clear_PG_WMdet_csv.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 30 16:06:42 2023
+
+@author: torterma
+
+Clean PamGuard  whistle and moan detector first detection of each wav file (might be very specific to Sylence data).
+This is because the first detection on each wav files corresponds to the detection of a electronic buzz made by the recorder
+
+"""
+
