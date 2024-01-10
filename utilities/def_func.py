8123d55fcdf9ff8f1848af989be650cdec36cc80
Maëlle TORTEROTOT <torterma@ensieta.ecole>
2024-01-10 09:11:43 +0100

    add arg message to get_csv_file

--- utilities/def_func.py
+++ utilities/def_func.py
@@ -23,7 +23,7 @@ import csv
 import warnings
 
 
-def get_csv_file(num_files: int) -> List[str]:
+def get_csv_file(num_files: int, message = 'Select csv') -> List[str]:
     '''Opens a file dialog multiple times
     to get X csv files (APLOSE formatted detection file, task status file...).
     Parameters :
@@ -37,7 +37,7 @@ def get_csv_file(num_files: int) -> List[str]:
     file_paths = []
     for _ in range(num_files):
         file_path = filedialog.askopenfilename(
-            title=f'Select csv ({len(file_paths) + 1}/{num_files})',
+            title=message + f' ({len(file_paths) + 1}/{num_files})',
             filetypes=[('CSV files', '*.csv')],
             parent=None
         )
