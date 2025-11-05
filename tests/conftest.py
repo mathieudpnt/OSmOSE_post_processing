e7a408bba182aca9b58e4afedab435c210ad2aeb
MaelleTtrt <48910568+MaelleTtrt@users.noreply.github.com>
2025-11-05 17:31:28 +0100

    changes related to fix of reshape_timebin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,16 @@
 import io
 import os
 from pathlib import Path
+from sqlite3.dbapi2 import Timestamp
 
 import numpy as np
 import pytest
 import soundfile as sf
 import yaml
 from osekit.utils.timestamp_utils import strftime_osmose_format
-from pandas import DataFrame, read_csv
+from pandas import DataFrame, read_csv, to_datetime
+
+from post_processing.utils.filtering_utils import get_timezone, read_dataframe
 
 SAMPLE = """dataset,filename,start_time,end_time,start_frequency,end_frequency,annotation,annotator,start_datetime,end_datetime,type,score
 sample_dataset,2025_01_25_06_20_00,0.0,10.0,0.0,72000.0,lbl2,ann2,2025-01-25T06:20:00.000+00:00,2025-01-25T06:20:10.000+00:00,WEAK,0.11
@@ -164,10 +167,11 @@ def sample_csv_timestamp(tmp_path: Path, sample_status: DataFrame) -> Path:
 
 
 @pytest.fixture
-def sample_yaml(tmp_path: Path,
-                           sample_csv_result: Path,
-                           sample_csv_timestamp: Path,
-                           ) -> Path:
+def sample_yaml(
+        tmp_path: Path,
+        sample_csv_result: Path,
+        sample_csv_timestamp: Path,
+) -> Path:
     yaml_content = {
         f"{sample_csv_result}": {
             "timebin_new": None,
@@ -177,6 +181,7 @@ def sample_yaml(tmp_path: Path,
             "annotation": "lbl1",
             "box": True,
             "timestamp_file": f"{sample_csv_timestamp}",
+            "filename_format": "%Y_%m_%d_%H_%M_%S",
             "user_sel": "all",
             "f_min": None,
             "f_max": None,
