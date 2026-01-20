87f291e042a3514bf986828f415cb3279c0c45c5
fouinel <fouin.elise@gmail.com>
2026-01-20 11:36:22 +0100

    change colors assignment

--- user_case/config.py
+++ user_case/config.py
@@ -8,4 +8,4 @@ config = yaml.safe_load(config_file.read_text()) if config_file.exists() else {}
 
 site_colors = config.get("site_colors", {"Site A Haute": "#118B50", "Site B Heugh": "#5DB996", "Site C Chat": "#B0DB9C", "Site D Simone": "#E3F0AF", "CA4": "#80D8C3", "Walde": "#4DA8DA", "Point C": "#932F67", "Point D": "#D92C54", "Point E": "#DDDEAB", "Point F": "#8ABB6C", "Point G": "#456882"})
 
-season_color = config.get("season_color", {"spring": "green", "summer": "darkgoldenrod", "autumn": "orange", "winter": "blue"})
+season_color = config.get("season_color", {"spring": "green", "summer": "orange", "autumn": "brown", "winter": "blue"})
